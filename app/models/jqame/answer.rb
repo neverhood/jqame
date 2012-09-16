58d0cf5163d9e6bab5267190d8ba23ff8406a2cc
Vlad Khomich <khomich.vlad@gmail.com>
2012-09-16 13:30:30 +0300

    instance variables instead of constants

--- app/models/jqame/answer.rb
+++ app/models/jqame/answer.rb
@@ -1,15 +1,19 @@
 module Jqame
   class Answer < ActiveRecord::Base
+    class << self
+      attr_accessor :max_answer_length, :max_comment_length
+    end
+
+    @max_answer_length  = 10000
+    @max_comment_length = 200
+
     attr_accessible :body, :full
 
     belongs_to :question
     has_many   :votes, :dependent => :destroy, :as => :votable
 
-    MAX_ANSWER_LENGTH = 10000
-    MAX_COMMENT_LENGTH = 200
-
-    validates :body, length: { within: (1..MAX_ANSWER_LENGTH) }, if: 'answer?'
-    validates :body, length: { within: (1..MAX_COMMENT_LENGTH) }, if: 'comment?'
+    validates :body, length: { within: (1..@max_answer_length) }, if: 'answer?'
+    validates :body, length: { within: (1..@max_comment_length) }, if: 'comment?'
 
     def comment?
       not full?
